7b956ed162be523521b2f06f3d29201ef9f4e421
Julian C. Dunn <jdunn@aquezada.com>
2016-02-17 18:25:46 -0500

    Added encryption options

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jdunn@aquezada.com'
 license          'Apache 2.0'
 description      'Set up an Asterisk server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'asterisk'
